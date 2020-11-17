a18bd6a1cf0e164c43aecbc098214338d178f8eb
Guilheme Freitas <freitas-gui19@hotmail.com>
2020-11-17 15:11:37 -0300

    updade and delete habit

--- habit/urls.py
+++ habit/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('home/', views.home, name="home"),
     path('create/', views.create_habit, name="create_habit"),
     path('show/all/', views.show_all, name="show_all"),
+    path('update/<int:pk>', views.update_habit, name="update_habit"),
+    path('delete/<int:pk>', views.delete_habit, name="delete_habit"),
+
+
 ]
--- habit/views.py
+++ habit/views.py
@@ -10,17 +10,41 @@ def home(request):
 
 def create_habit(request):
     ''' Form used for create a new habit. '''
+
     data = {}
-    form = FormHabit(request.POST)
+    form = FormHabit(request.POST or None)
 
     if form.is_valid():
         form.save()
-        return redirect('home')
+        return redirect('show_all')
     data['form'] = form
-    return render(request , 'show_all.html' , data)
+    return render(request , 'habit_form.html', data)
 
 def show_all(request):
+    ''' Show all habits that existed in db '''
+
     data = {}
     habits = Habit.objects.all()
     data['habits'] = habits
     return render(request, 'show_all.html',data)
+
+def update_habit(request, pk):
+    ''' Form used for update a habit already created. '''
+
+    data = {}
+    data['habit'] = Habit.objects.get(pk=pk)
+
+    form = FormHabit(request.POST  or None, instance = data['habit'])
+
+    if form.is_valid():
+        form.save()
+        return redirect('show_all')
+    data['form'] = form
+    return render(request , 'habit_form.html', data)
+
+def delete_habit(request, pk):
+    ''' Delete the habit with key = pk of db '''
+
+    habit = Habit.objects.get(pk=pk)
+    habit.delete()
+    return redirect('show_all')
